91df069dff0d77bdff7a3babe47577586c4bbfda
EchoDev <EchoDev@users.noreply.github.com>
2016-01-23 15:51:53 +0100

    Theather mode fix
    Dirty fix due to limitations, Theater mode replaced by tex

--- Twitch.user.js
+++ Twitch.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Twitch.tv Full HTML5 Player
-// @version      1.2.2
+// @version      1.2.1
 // @description  Twitch.tv Full HTML5 Player
 // @author       Devrim
 // @match        http://twitch.tv/*
@@ -8,24 +8,33 @@
 // @exclude      http://twitch.tv/*/v/*
 // @exclude      http://*.twitch.tv/*/v/*
 // @run-at       document-end
-// @grant        none
 // ==/UserScript==
 
-setTimeout(function() {
+
+setTimeout(function html5() {
     $(".js-player").html(
         $("<iframe>").attr({
-            "src": "http://player.twitch.tv/?branding=false&html5&showInfo=false&channel=" + $(location).attr('pathname'),
+            "src": "http://player.twitch.tv/?branding=false&html5&quality=source&showInfo=false&channel=" + $(location).attr('pathname'),
             "width": "100%",
             "height": "100%",
             "allowfullscreen": true,
             "webkitallowfullscreen": true,
-            "mozallowfullscreen": true
-        }).css(
+            "mozallowfullscreen": true,
+            
+            
+    }).css(
             "border", 0
         )
     );
     $(".player-overlay").hide(); 
     $(".player-loading").hide(); 
     $(".player-buttons-right").css("padding","0px 50px 0px 0px");
+    $('.ember-view.button.drop.action').text('Theater Mode');
+    $(".ember-view.button.drop.action").click(function enable() { 
+    $("#player").removeClass("dynamic-player");
+    $(".app-main").addClass("theatre");
+    $(".popup").css("display","none");
+      });
 
+    
 },5000);
