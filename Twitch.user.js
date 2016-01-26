50621c2a6b87dccb37b527b58309966aa6891335
EchoDev <EchoDev@users.noreply.github.com>
2016-01-26 21:48:02 +0100

    Reworked Theater Mode
    -Moved enter Theater Mode button to chat window
    -Theater Mode button should be more reliable.

--- Twitch.user.js
+++ Twitch.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Twitch.tv Full HTML5 Player
-// @version      1.3.0
+// @version      1.3.1
 // @description  Twitch.tv Full HTML5 Player
 // @author       Devrim
 // @match        http://twitch.tv/*
@@ -9,7 +9,6 @@
 // @exclude      http://*.twitch.tv/*/v/*
 // @run-at       document-end
 // ==/UserScript==
-
 
 setTimeout(function html5() {
     $(".js-player").html(
@@ -26,22 +25,18 @@ setTimeout(function html5() {
     );
     $(".player-overlay").hide(); 
     $(".player-loading").hide(); 
-    $('.ember-view.button.drop.action').text('Theater Mode');
-    $('.ember-view.button.drop.action').removeClass("drop","action");
-    $('.ember-view.button.action').removeClass("action");
-    $('.ember-view.button').addClass("primary");
-    $(".ember-view.button.primary").click(function enable() { 
+    var r= $('<button class="button primary float-left enter-theater-button"><span>Enter Theater Mode</span></button>');
+    $(".ember-chat .chat-interface .chat-buttons-container").append(r)
+    $(".enter-theater-button").toggle(function enable() {
     $("#player").removeClass("dynamic-player");
     $(".app-main").addClass("theatre");
     $(".popup").css("display","none");
     $(".conversations-content .conversations-list-container").css("display","none");
-    var r= $('<button class="button primary float-left exit-theater-button"><span>Exit Theater Mode</span></button>');
-    $(".ember-chat .chat-interface .chat-buttons-container").append(r)
-        $(".exit-theater-button").click(function exittheater() {   
+    $('.enter-theater-button').text('Exit Theater Mode');
+      }, function disable() {
     $("#player").addClass("dynamic-player");
     $(".app-main").removeClass("theatre");
-    $(".exit-theater-button").remove();
-    }); 
-    });
-
+    $('.enter-theater-button').text('Enter Theater Mode');
+        });
+          
 },5000);
