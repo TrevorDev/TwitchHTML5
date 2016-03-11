e405aba64f11a8b2b18a3ef1b06abedd87be547b
EchoDev <EchoDev@users.noreply.github.com>
2016-03-11 14:49:34 +0100

    Added support for host mode

--- Twitch.user.js
+++ Twitch.user.js
@@ -3,7 +3,7 @@ Thanks to https://github.com/BrockA for WaitForKeyElements.js
 **/
 // ==UserScript==
 // @name         Twitch.tv Full HTML5 Player
-// @version      1.5.1
+// @version      1.6.0
 // @description  Twitch.tv Full HTML5 Player
 // @author       Devrim
 // @include      /https?://(.*\.)?twitch.tv/
@@ -31,5 +31,39 @@ Thanks to https://github.com/BrockA for WaitForKeyElements.js
       }, function disable() {
     $("#player").addClass("dynamic-player");
     $(".app-main").removeClass("theatre");
+        });      
+}
+
+    waitForKeyElements("div[class^='dynamic-target-player']", html5host);
+    function html5host(){
+           var target = $(".target-user-and-game").text();
+           var trimmed = $.trim(target);
+           var host = trimmed.split(" ",1);   
+           $("[class^='dynamic-target-player']").html($("<iframe>").attr({
+            "src": location.protocol + "//player.twitch.tv/?branding=false&html5&quality=source&showInfo=false&channel=" + (host),
+            "width": "100%",
+            "height": "100%",
+            "allowfullscreen": true,
+            "webkitallowfullscreen": true,
+            "mozallowfullscreen": true,            
+    }).css(
+            "border", 0
+        )
+    );
+    var r= $('<button class="button primary float-left enter-theater-button-host"><span>Theater Mode</span></button>');
+    $(".enter-theater-button").remove();
+    $(".ember-chat .chat-interface .chat-buttons-container").append(r)
+    $(".enter-theater-button-host").toggle(function enable() {
+    $(".app-main").addClass("theatre");
+    $(".dynamic-target-player").addClass("target-player");
+    $(".dynamic-target-player").removeClass("dynamic-target-player");
+    $(".dynamic-player").removeClass("dynamic-player");
+    $(".conversations-content .conversations-list-container").css("display","none");
+      }, function disable() {
+    $(".app-main").removeClass("theatre");
+    $(".target-player").addClass("dynamic-player");
+    $(".target-player").addClass("dynamic-target-player");
+    $(".target-player").removeClass("target-player");
+
         });      
 }
