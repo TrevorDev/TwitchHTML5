ca3a130b2aa523ea3444ffd02eaa655730dea5a6
EchoDev <EchoDev@users.noreply.github.com>
2016-04-05 17:44:46 +0200

    Add VOD support
    -Add VOD support
    -Changes to parameters for possible Twitch changes in the future

--- Twitch.user.js
+++ Twitch.user.js
@@ -3,7 +3,7 @@ Thanks to https://github.com/BrockA for WaitForKeyElements.js
 **/
 // ==UserScript==
 // @name         Twitch.tv Full HTML5 Player
-// @version      1.6.2
+// @version      1.7.0
 // @description  Twitch.tv Full HTML5 Player
 // @author       Devrim
 // @include      /https?://(.*\.)?twitch.tv/
@@ -12,7 +12,7 @@ Thanks to https://github.com/BrockA for WaitForKeyElements.js
     waitForKeyElements("div[class^='js-player']", html5);
     function html5(){
            $("[class^='js-player']").html($("<iframe>").attr({
-            "src": location.protocol + "//player.twitch.tv/?branding=false&html5&quality=source&showInfo=false&channel=" + $(location).attr('pathname'),
+            "src": location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + $(location).attr('pathname'),
             "width": "100%",
             "height": "100%",
             "allowfullscreen": true,
@@ -40,7 +40,7 @@ Thanks to https://github.com/BrockA for WaitForKeyElements.js
            var trimmed = $.trim(target);
            var host = trimmed.split(" ",1);   
            $("[class^='dynamic-target-player']").html($("<iframe>").attr({
-            "src": location.protocol + "//player.twitch.tv/?branding=false&html5&quality=source&showInfo=false&channel=" + (host),
+            "src": location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + (host),
             "width": "100%",
             "height": "100%",
             "allowfullscreen": true,
@@ -69,16 +69,33 @@ Thanks to https://github.com/BrockA for WaitForKeyElements.js
         });      
 }
 
-    waitForKeyElements("div[class^='live_frontpage_player_container']", html5fp);
-    function html5fp(){
-           var fp = $(".active").find(".name").text();
-           $("[class^='live_frontpage_player_container']").html($("<iframe>").attr({
-            "src": location.protocol + "//player.twitch.tv/?branding=false&html5&quality=source&showInfo=false&channel=" + (fp),
+
+    waitForKeyElements("div[class^='archive_info']", html5vod);
+    function html5vod(){
+           $("[class^='dynamic-player']").html($("<iframe>").attr({
+            "src": location.protocol + "//player.twitch.tv/?html5&!branding&!showInfo&video=v" +  window.location.href.split('/').pop(),
             "width": "100%",
             "height": "100%",
             "allowfullscreen": true,
             "webkitallowfullscreen": true,
             "mozallowfullscreen": true,            
+    }).css(
+            "border", 0
+        )
+    );
+    }
+
+
+    waitForKeyElements("div[class^='live_frontpage_player_container']", html5fp);
+    function html5fp(){
+           var fp = $(".active").find(".name").text();
+           $("[class^='live_frontpage_player_container']").html($("<iframe>").attr({
+            "src": location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + (fp),
+            "width": "100%",
+            "height": "100%",
+            "allowfullscreen": true,
+            "webkitallowfullscreen": true,
+            "mozallowfullscreen": true, 
     }).css(
             "border", 0
         )
