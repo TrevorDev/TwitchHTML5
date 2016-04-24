7e9316a93a114398183d4995a18f529f1310f091
vBm <the.vbm@gmail.com>
2016-04-24 11:36:55 +0200

    ESLint: remove unnecessarily quoted properties

--- Twitch.user.js
+++ Twitch.user.js
@@ -14,12 +14,12 @@ waitForKeyElements("div[class^='js-player']", html5);
 function html5() {
     $("[class^='js-player']").html(
         $("<iframe>").attr({
-            "src": location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + $(location).attr('pathname'),
-            "width": "100%",
-            "height": "100%",
-            "allowfullscreen": true,
-            "webkitallowfullscreen": true,
-            "mozallowfullscreen": true,
+            src: location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + $(location).attr('pathname'),
+            width: "100%",
+            height: "100%",
+            allowfullscreen: true,
+            webkitallowfullscreen: true,
+            mozallowfullscreen: true,
         }).css(
             "border", 0
         )
@@ -43,12 +43,12 @@ function html5host() {
     var host = trimmed.split(" ", 1);
     $("[class^='dynamic-target-player']").html(
         $("<iframe>").attr({
-            "src": location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + (host),
-            "width": "100%",
-            "height": "100%",
-            "allowfullscreen": true,
-            "webkitallowfullscreen": true,
-            "mozallowfullscreen": true,
+            src: location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + (host),
+            width: "100%",
+            height: "100%",
+            allowfullscreen: true,
+            webkitallowfullscreen: true,
+            mozallowfullscreen: true,
         }).css(
             "border", 0
         )
@@ -75,12 +75,12 @@ waitForKeyElements("div[class^='archive_info']", html5vod);
 function html5vod() {
     $("[class^='dynamic-player']").html(
         $("<iframe>").attr({
-            "src": location.protocol + "//player.twitch.tv/?html5&!branding&!showInfo&video=v" + window.location.href.split('/').pop(),
-            "width": "100%",
-            "height": "100%",
-            "allowfullscreen": true,
-            "webkitallowfullscreen": true,
-            "mozallowfullscreen": true,
+            src: location.protocol + "//player.twitch.tv/?html5&!branding&!showInfo&video=v" + window.location.href.split('/').pop(),
+            width: "100%",
+            height: "100%",
+            allowfullscreen: true,
+            webkitallowfullscreen: true,
+            mozallowfullscreen: true,
         }).css(
             "border", 0
         )
@@ -92,12 +92,12 @@ function html5fp() {
     var fp = $(".active").find(".name").text();
     $("[class^='live_frontpage_player_container']").html(
         $("<iframe>").attr({
-            "src": location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + (fp),
-            "width": "100%",
-            "height": "100%",
-            "allowfullscreen": true,
-            "webkitallowfullscreen": true,
-            "mozallowfullscreen": true,
+            src: location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + (fp),
+            width: "100%",
+            height: "100%",
+            allowfullscreen: true,
+            webkitallowfullscreen: true,
+            mozallowfullscreen: true,
         }).css(
             "border", 0
         )
