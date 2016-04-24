0bb9d15f6d27cf8765d8189015e72cd7d233c907
vBm <the.vbm@gmail.com>
2016-04-24 11:22:11 +0200

    Cosmetic changes: - remove whitespace - proper indentation

--- Twitch.user.js
+++ Twitch.user.js
@@ -9,44 +9,47 @@ Thanks to https://github.com/BrockA for WaitForKeyElements.js
 // @include      /https?://(.*\.)?twitch.tv/
 // @require      https://github.com/EchoDev/TwitchHTML5/raw/master/waitForKeyElements.js
 // ==/UserScript==
-    waitForKeyElements("div[class^='js-player']", html5);
-    function html5(){
-           $("[class^='js-player']").html($("<iframe>").attr({
+
+waitForKeyElements("div[class^='js-player']", html5);
+function html5(){
+    $("[class^='js-player']").html(
+        $("<iframe>").attr({
             "src": location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + $(location).attr('pathname'),
             "width": "100%",
             "height": "100%",
             "allowfullscreen": true,
             "webkitallowfullscreen": true,
-            "mozallowfullscreen": true,            
-    }).css(
+            "mozallowfullscreen": true,
+        }).css(
             "border", 0
         )
     );
     var r= $('<button class="button primary float-left enter-theater-button"><span>Theater Mode</span></button>');
     $(".ember-chat .chat-interface .chat-buttons-container").append(r)
     $(".enter-theater-button").toggle(function enable() {
-    $("#player").removeClass("dynamic-player");
-    $(".app-main").addClass("theatre");
-    $(".conversations-content .conversations-list-container").css("display","none");
-      }, function disable() {
-    $("#player").addClass("dynamic-player");
-    $(".app-main").removeClass("theatre");
-        });      
+        $("#player").removeClass("dynamic-player");
+        $(".app-main").addClass("theatre");
+        $(".conversations-content .conversations-list-container").css("display","none");
+    }, function disable() {
+        $("#player").addClass("dynamic-player");
+        $(".app-main").removeClass("theatre");
+    });
 }
 
-    waitForKeyElements("div[class^='dynamic-target-player']", html5host);
-    function html5host(){
-           var target = $(".target-user-and-game").text();
-           var trimmed = $.trim(target);
-           var host = trimmed.split(" ",1);   
-           $("[class^='dynamic-target-player']").html($("<iframe>").attr({
+waitForKeyElements("div[class^='dynamic-target-player']", html5host);
+function html5host(){
+    var target = $(".target-user-and-game").text();
+    var trimmed = $.trim(target);
+    var host = trimmed.split(" ",1);
+    $("[class^='dynamic-target-player']").html(
+        $("<iframe>").attr({
             "src": location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + (host),
             "width": "100%",
             "height": "100%",
             "allowfullscreen": true,
             "webkitallowfullscreen": true,
-            "mozallowfullscreen": true,            
-    }).css(
+            "mozallowfullscreen": true,
+        }).css(
             "border", 0
         )
     );
@@ -55,50 +58,48 @@ Thanks to https://github.com/BrockA for WaitForKeyElements.js
     $(".app-main").removeClass("theatre");
     $(".ember-chat .chat-interface .chat-buttons-container").append(r)
     $(".enter-theater-button-host").toggle(function enable() {
-    $(".app-main").addClass("theatre");
-    $(".dynamic-target-player").addClass("target-player");
-    $(".dynamic-target-player").removeClass("dynamic-target-player");
-    $(".dynamic-player").removeClass("dynamic-player");
-    $(".conversations-content .conversations-list-container").css("display","none");
-      }, function disable() {
-    $(".app-main").removeClass("theatre");
-    $(".target-player").addClass("dynamic-player");
-    $(".target-player").addClass("dynamic-target-player");
-    $(".target-player").removeClass("target-player");
-
-        });      
+        $(".app-main").addClass("theatre");
+        $(".dynamic-target-player").addClass("target-player");
+        $(".dynamic-target-player").removeClass("dynamic-target-player");
+        $(".dynamic-player").removeClass("dynamic-player");
+        $(".conversations-content .conversations-list-container").css("display","none");
+    }, function disable() {
+        $(".app-main").removeClass("theatre");
+        $(".target-player").addClass("dynamic-player");
+        $(".target-player").addClass("dynamic-target-player");
+        $(".target-player").removeClass("target-player");
+    });
 }
 
-
-    waitForKeyElements("div[class^='archive_info']", html5vod);
-    function html5vod(){
-           $("[class^='dynamic-player']").html($("<iframe>").attr({
+waitForKeyElements("div[class^='archive_info']", html5vod);
+function html5vod(){
+    $("[class^='dynamic-player']").html(
+        $("<iframe>").attr({
             "src": location.protocol + "//player.twitch.tv/?html5&!branding&!showInfo&video=v" + window.location.href.split('/').pop(),
             "width": "100%",
             "height": "100%",
             "allowfullscreen": true,
             "webkitallowfullscreen": true,
-            "mozallowfullscreen": true,            
-    }).css(
+            "mozallowfullscreen": true,
+        }).css(
             "border", 0
         )
     );
-    }
+}
 
-
-    waitForKeyElements("div[class^='live_frontpage_player_container']", html5fp);
-    function html5fp(){
-           var fp = $(".active").find(".name").text();
-           $("[class^='live_frontpage_player_container']").html($("<iframe>").attr({
+waitForKeyElements("div[class^='live_frontpage_player_container']", html5fp);
+function html5fp(){
+    var fp = $(".active").find(".name").text();
+    $("[class^='live_frontpage_player_container']").html(
+        $("<iframe>").attr({
             "src": location.protocol + "//player.twitch.tv/?!branding&html5&quality=source&!showInfo&channel=" + (fp),
             "width": "100%",
             "height": "100%",
             "allowfullscreen": true,
             "webkitallowfullscreen": true,
-            "mozallowfullscreen": true, 
-    }).css(
+            "mozallowfullscreen": true,
+        }).css(
             "border", 0
         )
     );
-
-        };      
+};
